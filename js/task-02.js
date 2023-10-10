8e6fec118024984fc7b0d8d3b8076b47f87b4355
Ilya Bondarchuk <cod3provider@gmail.com>
2023-10-10 14:07:39 +0300

    second task done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+    'Potatoes',
+    'Mushrooms',
+    'Garlic',
+    'Tomatos',
+    'Herbs',
+    'Condiments',
 ];
+
+const list = document.querySelector('#ingredients');
+
+const createItems = arr => {
+    const createli = arr.map(item => {
+        const li = document.createElement('li');
+        li.textContent = item;
+        li.classList.add('item');
+        return li;
+    })
+    list.append(...createli);
+}
+createItems(ingredients);
